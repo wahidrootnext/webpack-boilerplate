bf31c94f62f1d1832f29abbf91fa7ab9fc1152bd
Mohammad Wahid <wahid.warid55@gmail.com>
2021-03-11 15:11:02 +0600

    Bootstrap's variables attached, dropdown, navbar script modified for visibility on click inside/outside

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,19 @@
 import 'jquery';
 import '@popperjs/core';
 import 'bootstrap';
+
+
+//Disable dropdown hide during inside click
+$('[data-bs-disable-inside="true"]').closest('.dropdown').find('.dropdown-menu').click(function (e) {
+	e.stopPropagation();
+});
+
+
+//Close navigation on outside click
+$(document).click(function (event) {
+	let clickedElement = $(event.target),
+		isNavbarOpen = $(".navbar-collapse").hasClass("show");
+	if (isNavbarOpen === true && !clickedElement.hasClass("navbar-toggler")) {
+		$(".navbar-toggler").click();
+	}
+});
